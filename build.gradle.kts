059ea537334eb4a640e7756fb5dc89c642b7e574
choijunghwan <vkdlxj3562@naver.com>
2023-06-27 16:46:07 +0900

    feat: 파일로 부터 데이터를 읽어 디비 저장하는 잡 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-batch")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-quartz")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.modelmapper:modelmapper:2.1.1")
 	runtimeOnly("com.h2database:h2")
-	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("mysql:mysql-connector-java:8.0.22")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.batch:spring-batch-test")
 }
---
+++ src/main/java/com/study/batch/demo/batch/chunk/processor/FileItemProcessor.java
@@ -0,0 +1,16 @@
+package com.study.batch.demo.batch.chunk.processor;
+
+import com.study.batch.demo.batch.domain.Product;
+import com.study.batch.demo.batch.domain.ProductVO;
+import org.modelmapper.ModelMapper;
+import org.springframework.batch.item.ItemProcessor;
+
+public class FileItemProcessor implements ItemProcessor<ProductVO, Product> {
+    @Override
+    public Product process(ProductVO item) throws Exception {
+        ModelMapper modelMapper = new ModelMapper();
+        Product product = modelMapper.map(item, Product.class);
+
+        return product;
+    }
+}
